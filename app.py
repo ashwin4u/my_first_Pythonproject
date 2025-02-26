992c8eff1a546b853aa6da4ca9dd3954efdadaaa
unknown <ashwinbdata@gmail.com>
2025-02-26 22:02:49 +0530

    Example for Return Statement

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
-# app.py
-def say_hello():
-    print("Hello, Jenkins!. I Added this change. Happy Learning")
-    print("Exploring Jenkins!")
-    print("Jenkins is Awesome Tool")
 
-if __name__ == "__main__":
-    print(say_hello())
+#return can Return any type of data.
+
+def return_manytypes_data(data):
+    """Example for demonstrating that "return" can return any Type of Data"""
+    if isinstance(data, int):
+        return data + 5
+    elif isinstance(data, float):
+        return data * 2
+    elif isinstance(data, str):
+        return data.upper()
+    elif isinstance(data, bool):
+        return not data
+    elif isinstance(data, (list, tuple, set)):
+        return None
+    else:
+        return None
+
+
+
+res=return_manytypes_data("Ashwin")
+print(res)
