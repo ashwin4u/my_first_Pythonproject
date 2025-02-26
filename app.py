0702f75d8fa9d92d54dc420657b53c172bb3c13d
unknown <ashwinbdata@gmail.com>
2025-02-26 21:48:42 +0530

    New Print is added

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # app.py
 def say_hello():
-    return "Hello, Jenkins!. Ashwin has Added this change. Happy Learning"
+    print("Hello, Jenkins!. I Added this change. Happy Learning")
+    print("Exploring Jenkins!")
 
 if __name__ == "__main__":
     print(say_hello())
