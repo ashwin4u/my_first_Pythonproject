dba9a2703b98ee004ce529256b855e8b973f7b83
unknown <ashwinbdata@gmail.com>
2025-02-27 21:03:26 +0530

    Hello update

--- app.py
+++ app.py
@@ -21,7 +21,6 @@ def return_manytypes_data(data):
 res=return_manytypes_data("Ashwin")
 print(res)
 print("The Result is:",res)
-print("Good Evening Everyone.Happy Learning")
-print("Hello")
+print("Hello All")
 
 
