41757881c9d5d5eecefabea1e5421a10b0441c16
unknown <ashwinbdata@gmail.com>
2025-02-27 21:05:37 +0530

    Hello update

--- app.py
+++ app.py
@@ -1,26 +1,4 @@
+def greet():
+    print("GM")
 
-#return can Return any type of data.
-
-def return_manytypes_data(data):
-    """Example for demonstrating that "return" can return any Type of Data"""
-    if isinstance(data, int):
-        return data + 5
-    elif isinstance(data, float):
-        return data * 2
-    elif isinstance(data, str):
-        return data.upper()
-    elif isinstance(data, bool):
-        return not data
-    elif isinstance(data, (list, tuple, set)):
-        return None
-    else:
-        return None
-
-
-
-res=return_manytypes_data("Ashwin")
-print(res)
-print("The Result is:",res)
-print("Hello All")
-
-
+greet()
