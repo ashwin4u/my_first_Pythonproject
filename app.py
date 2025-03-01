bef2fad587f319b537e23ff417ffbfb3460a69f8
unknown <ashwinbdata@gmail.com>
2025-03-01 14:58:20 +0530

    CSE A did this Project

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ def say_hello():
 if __name__ == "__main__":
     print(say_hello())
     print("Project has completed")
-    print("Sreeya done Testing Part"
+    print("CSE-A Students Completed this Project")
 
