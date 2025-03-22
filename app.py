8ce4c60aa4d611990dc952ff51dbafae34c7513a
unknown <ashwinbdata@gmail.com>
2025-03-22 14:26:02 +0530

    Hello ddjdshfd

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ def say_hello():
 if __name__ == "__main__":
     print(say_hello())
     print("It's completed. Now it's time for Testing and Building")
-    print("I updated")
+    print("I updated now")
 
 
