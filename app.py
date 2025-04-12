72cffb376a2ee206561900d548c60b8ce1bfad6f
unknown <ashwinbdata@gmail.com>
2025-04-12 14:14:36 +0530

    Avinash did

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ def say_hello():
 
 if __name__ == "__main__":
     print(say_hello())
-    print("It's completed. Now it's time for Testing and Building")
-    print("I upd")
+    print("Avinash changed")
+
 
 
