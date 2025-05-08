ca2b2f61060be08f39fa836028eb2f8f64724f13
unknown <ashwinbdata@gmail.com>
2025-05-08 12:14:02 +0530

    I did some change

--- app.py
+++ app.py
@@ -3,9 +3,8 @@ def say_hello():
 
 if __name__ == "__main__":
     print(say_hello())
-    print("Avinash changed")
-    print("I completed it")
-    print("Done")
+    print("Today us May 8th")
+    
 
 
 
